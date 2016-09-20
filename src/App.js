0f19bb8f449e4fc594d3cac5daa47a92f7c47b33
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-09-20 15:54:18 -0400

    updated field form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+class App extends React.Component {
+    state = {
+    location: ''
+  };
+
+  fetchData = (evt) => {
+    evt.preventDefault();
+    console.log('fetch data for', this.state.location);
+  };
+
+    changeLocation = (evt) => {
+    this.setState({
+      location: evt.target.value
+    });
+  };
+
+render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Weather</h1>
+        <form onSubmit={this.fetchData}>
+          <label>I want to know the weather for
+            <input
+              placeholder={"City, Country"}
+              type="text"
+              value={this.state.location}
+              onChange={this.changeLocation}
+            />
+          </label>
+        </form>
       </div>
     );
   }
+  
 }
 
 export default App;
