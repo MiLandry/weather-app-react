84686e1be8b4ff0579384eb97f90cda7875d8015
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-09-26 12:53:08 -0400

    added random stuff if app dont work this is why

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './App.css';
 import xhr from 'xhr';
-import Plot from './Plot';
+import { connect } from 'react-redux';
 
+import Plot from './Plot';
+import {
+  changeLocation,
+  setData,
+  setDates,
+  setTemps,
+  setSelectedDate,
+  setSelectedTemp
+} from './actions';
 
 class App extends React.Component {
-  state = {
-    location: '',
-    data: {},
-    dates: [],
-    temps: []
-  };
-
   fetchData = (evt) => {
     evt.preventDefault();
 
-    var location = encodeURIComponent(this.state.location);
+    var location = encodeURIComponent(this.props.location);
 
     var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
-    var urlSuffix = '&APPID=d5bcb5d2af182c505e66dd7b13cdb10b&units=imperial';
+    var urlSuffix = '&APPID=dbe69e56e7ee5f981d76c3e77bbb45c0&units=metric';
     var url = urlPrefix + location + urlSuffix;
 
     var self = this;
@@ -26,6 +28,7 @@ class App extends React.Component {
     xhr({
       url: url
     }, function (err, data) {
+
       var body = JSON.parse(data.body);
       var list = body.list;
       var dates = [];
@@ -35,60 +38,74 @@ class App extends React.Component {
         temps.push(list[i].main.temp);
       }
 
-      self.setState({
-        data: body,
-        dates: dates,
-        temps: temps
-      });
-    });
-
-  }
-
-  changeLocation = (evt) => {
-    this.setState({
-      location: evt.target.value
+      self.props.dispatch(setData(body));
+      self.props.dispatch(setDates(dates));
+      self.props.dispatch(setTemps(temps));
+      self.props.dispatch(setSelectedDate(''));
+      self.props.dispatch(setSelectedTemp(null));
     });
   };
 
-render() {
+  onPlotClick = (data) => {
+    if (data.points) {
+      var number = data.points[0].pointNumber;
+      this.props.dispatch(setSelectedDate(this.props.dates[number]));
+      this.props.dispatch(setSelectedTemp(this.props.temps[number]))
+    }
+  };
+
+  changeLocation = (evt) => {
+    this.props.dispatch(changeLocation(evt.target.value));
+  };
+
+  render() {
     var currentTemp = 'not loaded yet';
-    if (this.state.data.list) {
-      currentTemp = this.state.data.list[0].main.temp;
+    if (this.props.data.list) {
+      currentTemp = this.props.data.list[0].main.temp;
     }
     return (
       <div>
         <h1>Weather</h1>
         <form onSubmit={this.fetchData}>
-          <label>I want to know the weather for
+          <label>City, Country
             <input
               placeholder={"City, Country"}
               type="text"
-              value={this.state.location}
+              value={this.props.location}
               onChange={this.changeLocation}
             />
           </label>
         </form>
-
-{/*only render the forecast if data is there*/}
-        {(this.state.data.list) ? (
-        <div className="wrapper">
-          <p className="temp-wrapper">
-            <span className="temp">{ currentTemp }</span>
-            <span className="temp-symbol">°C</span>
-          </p>
-          <h2>Forecast</h2>
-          <Plot
-            xData={this.state.dates}
-            yData={this.state.temps}
-            type="scatter"
-          />
-        </div>
+        {/*
+          Render the current temperature and the forecast if we have data
+          otherwise return null
+        */}
+        {(this.props.data.list) ? (
+          <div>
+            {/* Render the current temperature if no specific date is selected */}
+            {(this.props.selected.temp) ? (
+              <p>The temperature on { this.props.selected.date } will be { this.props.selected.temp }°C</p>
+            ) : (
+              <p>The current temperature is { currentTemp }°C!</p>
+            )}
+            <h2>Forecast</h2>
+            <Plot
+              xData={this.props.dates}
+              yData={this.props.temps}
+              onPlotClick={this.onPlotClick}
+              type="scatter"
+            />
+          </div>
         ) : null}
 
       </div>
     );
   }
-
 }
 
-export default App;
+// Since we want to have the entire state anyway, we can simply return it as is!
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps)(App);
